ef7a85f1822ba0a477deea7c86bbf932746a34a7
Graham Lowe <graham.lowe@gmail.com>
2018-07-26 22:06:35 -0400

    Clarify names

--- p2/src/lib.rs
+++ p2/src/lib.rs
@@ -5,10 +5,10 @@
 ///
 /// Examples:
 /// ```
-/// use p2::products;
-/// assert_eq!(products(&[1, 2, 3, 4, 5]), [120, 60, 40, 30, 24]);
+/// use p2::products_with_division;
+/// assert_eq!(products_with_division(&[1, 2, 3, 4, 5]), [120, 60, 40, 30, 24]);
 /// ```
-pub fn products(numbers: &[u32]) -> Vec<u32> {
+pub fn products_with_division(numbers: &[u32]) -> Vec<u32> {
     let product_of_all: u32 = numbers.iter().product();
     numbers.iter().map(|n| product_of_all / n).collect()
 }
@@ -20,10 +20,10 @@ pub fn products(numbers: &[u32]) -> Vec<u32> {
 ///
 /// Examples:
 /// ```
-/// use p2::products_without_div;
-/// assert_eq!(products_without_div(&[1, 2, 3, 4, 5]), [120, 60, 40, 30, 24]);
+/// use p2::products_without_division;
+/// assert_eq!(products_without_division(&[1, 2, 3, 4, 5]), [120, 60, 40, 30, 24]);
 /// ```
-pub fn products_without_div(numbers: &[u32]) -> Vec<u32> {
+pub fn products_without_division(numbers: &[u32]) -> Vec<u32> {
     (0..numbers.len())
         .map(|i| {
             numbers
@@ -37,35 +37,23 @@ pub fn products_without_div(numbers: &[u32]) -> Vec<u32> {
 
 #[cfg(test)]
 mod tests {
-    use products;
-    use products_without_div;
-
     #[test]
-    fn test_five() {
-        let input = [1, 2, 3, 4, 5];
-        let output = [120, 60, 40, 30, 24];
-        assert_eq!(products(&input), output);
+    fn test_with_division() {
+        use products_with_division;
+        assert_eq!(
+            products_with_division(&[1, 2, 3, 4, 5]),
+            [120, 60, 40, 30, 24]
+        );
+        assert_eq!(products_with_division(&[3, 2, 1]), [2, 3, 6]);
     }
 
     #[test]
-    fn test_five_without_div() {
-        let input = [1, 2, 3, 4, 5];
-        let output = [120, 60, 40, 30, 24];
-        assert_eq!(products_without_div(&input), output);
+    fn test_without_division() {
+        use products_without_division;
+        assert_eq!(
+            products_without_division(&[1, 2, 3, 4, 5]),
+            [120, 60, 40, 30, 24]
+        );
+        assert_eq!(products_without_division(&[3, 2, 1]), [2, 3, 6]);
     }
-
-    #[test]
-    fn test_three() {
-        let input = [3, 2, 1];
-        let output = [2, 3, 6];
-        assert_eq!(products(&input), output);
-    }
-
-    #[test]
-    fn test_three_without_div() {
-        let input = [3, 2, 1];
-        let output = [2, 3, 6];
-        assert_eq!(products_without_div(&input), output);
-    }
-
 }
