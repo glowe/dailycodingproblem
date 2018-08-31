eb5ebecd222cad4023a1f85cdfed99bd2cfc0fb9
Graham Lowe <graham.lowe@gmail.com>
2018-08-30 21:21:48 -0400

    Add dp solution for problem #19 and cleanup

--- p019/src/lib.rs
+++ p019/src/lib.rs
@@ -7,37 +7,76 @@ minimizing cost while ensuring that no two neighboring houses are of the same co
 Given an N by K matrix where the nth row and kth column represents the cost to build the nth house
 with kth color, return the minimum cost which achieves this goal.
 */
-fn min_cost_recur(n: usize, last_k: usize, costs: &Vec<Vec<u64>>) -> u64 {
+
+fn min_cost_dp(costs: &Vec<Vec<u64>>) -> u64 {
     let num_colors = costs[0].len();
-    if n == costs.len() {
-        // We've painted all the houses
-        return 0;
-    }
-    // the minimum cost to paint this house is the mininum of painting this house some color +
-    // the mininum cost of paiting all the other houses
-    let mut min = std::u64::MAX;
+    let num_houses = costs.len();
+    let mut cache = vec![vec![std::u64::MAX; num_colors]; num_houses];
+    // Initialize first row for cache.
     for k in 0..num_colors {
-        if k == last_k {
-            // We can't paint this house the same color as the neighbour
-            continue;
-        }
-        let cost = costs[n][k] + min_cost_recur(n + 1, k, costs);
-        min = min.min(cost);
+        cache[0][k] = costs[0][k];
     }
-    min
+    // Fill the rest.
+    for house in 1..num_houses {
+        for color in 0..num_colors {
+            for neighbor_color in 0..num_colors {
+                if neighbor_color == color {
+                    continue;
+                }
+                cache[house][color] =
+                    cache[house][color].min(costs[house][color] + cache[house - 1][neighbor_color]);
+            }
+        }
+    }
+    // Min cost will be in final row.
+    *cache[num_houses - 1].iter().min().unwrap()
 }
 
 fn min_cost(costs: &Vec<Vec<u64>>) -> u64 {
+    fn recur(house: usize, neighbor_color: usize, costs: &Vec<Vec<u64>>) -> u64 {
+        let num_colors = costs[0].len();
+        let num_houses = costs.len();
+        if house == num_houses {
+            // We've painted all the houses
+            return 0;
+        }
+        // the minimum cost to paint this house is: the mininum of painting this house some color +
+        // the mininum cost of painting all the other houses
+        let mut min = std::u64::MAX;
+        for color in 0..num_colors {
+            if color == neighbor_color {
+                // We can't paint this house the same color as the neighbour
+                continue;
+            }
+            min = min.min(costs[house][color] + recur(house + 1, color, costs))
+        }
+        min
+    }
+    // This is not a legal color. We use this to avoid special casing the first house.
     let sentinel_color = costs[0].len();
-    min_cost_recur(0, sentinel_color, costs)
+    recur(0, sentinel_color, costs)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn costs() -> Vec<Vec<u64>> {
+        vec![
+            vec![40, 50, 60],
+            vec![10, 90, 60],
+            vec![30, 20, 5],
+            vec![10, 20, 30],
+        ]
+    }
+
     #[test]
-    fn it_works() {
-        let costs = vec![vec![40, 50, 60], vec![10, 90, 60], vec![30, 20, 5]];
-        assert_eq!(min_cost(&costs), 65);
+    fn test_min_cost() {
+        assert_eq!(min_cost(&costs()), 75);
+    }
+
+    #[test]
+    fn test_min_cost_dp() {
+        assert_eq!(min_cost_dp(&costs()), 75);
     }
 }
